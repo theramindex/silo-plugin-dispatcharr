27226a9b9fedced0a67b20f8acd914f1203c1cc4
ramindex-ci <github@ramindex.org>
2026-09-09 00:21:22 -0400

    Update My TV search expectation for channel favorites

--- internal/plugin/routes_test.go
+++ internal/plugin/routes_test.go
@@ -287,7 +287,7 @@ func TestHTTPRoutesServerAppPageIncludesVirtualFolderDrilldown(t *testing.T) {
 		`data-view="mytv"`,
 		`function renderMyTVPage()`,
 		`function myTVSportsPeople()`,
-		`Find shows, teams, leagues, or events to follow`,
+		`Find channels, shows, teams, or events`,
 		`My TV will watch future guide updates`,
 		`Favorites <small id="favorite-count">0</small>`,
 		`<span>Sports</span>`,
